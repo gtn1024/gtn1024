7ad647ec22c94f910fbedc200ca678deddc801e9
Taoning Ge <gtn1024o@outlook.com>
2023-10-02 19:24:39 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+</script>
+
+<template>
+  <main h-screen w-full>
+    <h1>Hi, there 👋</h1>
+  </main>
+</template>
+
+<style scoped>
+</style>
---
+++ uno.config.js
@@ -0,0 +1,18 @@
+import {
+  defineConfig,
+  presetAttributify,
+  presetIcons,
+  presetTypography,
+  presetUno,
+} from 'unocss'
+
+export default defineConfig({
+  presets: [
+    presetUno(),
+    presetAttributify(),
+    presetIcons({
+      extraProperties: { 'display': 'inline-block', 'vertical-align': 'middle' },
+    }),
+    presetTypography(),
+  ],
+})
