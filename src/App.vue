5d4edef760cce74513355458327bdfbbb23726ca
Taoning Ge <gtn1024o@outlook.com>
2024-02-08 18:59:21 +0800

    重写个人主页

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,12 @@
 <script setup>
+import MainContainer from './components/MainContainer.vue';
+
 </script>
 
 <template>
-  <div flex max-w-1200px mx-auto items-start pt-4 gap-8>
-    <div w="3/4" bg="#fff">
-      <div flex pt-4 pl-4 pb-8 gap-4 bg="#267ad1">
-        <div>
-          <img src="https://avatars.githubusercontent.com/u/69113276" alt="avatar" width="120" rounded-60px />
-        </div>
-        <div flex-auto text-white>
-          <h1 text-8 leading-14>
-            gtn1024
-          </h1>
-          <p>
-            啥都不会...
-          </p>
-        </div>
-      </div>
-      <div p-4>
-        <h2 text-6 pb-2>个人简介</h2>
-        <p>
-          博客：<a href="https://blog.gtn1024.me" target="_blank">https://blog.gtn1024.me</a>
-        </p>
-        <br />
-        <img src="https://github-readme-stats.vercel.app/api?username=gtn1024&show_icons=true"
-          alt="gtn1024's github stats" title="gtn1024's github stats" />
-      </div>
-    </div>
-    <div w="1/4" bg="#fff">
-      <div w-full>
-        <div text-5 flex justify-between items-center m-2>
-          <div flex items-center gap-1 leading-8>
-            <span class="i-bxs:user" /> 联系方式
-          </div>
-          <span class="i-icon-park:down" />
-        </div>
-        <div>
-          <div flex justify-between items-center m-2 hover:bg="#f4f4f4">
-            <div flex items-center gap-1 ml-6 leading-8>
-              <span class="i-material-symbols:mail-outline" />
-              <a href="mailto:i@gtn1024.me" target="_blank">
-                i@gtn1024.me
-              </a>
-            </div>
-          </div>
-          <div flex justify-between items-center m-2 hover:bg="#f4f4f4">
-            <div flex items-center gap-1 ml-6 leading-8>
-              <span class="i-mdi:github" />
-              <a href="https://github.com/gtn1024" target="_blank">
-                gtn1024
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
+  <div h-screen flex justify-center items-center>
+    <div>
+      <MainContainer />
     </div>
   </div>
 </template>
---
+++ src/components/MainContainer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div flex flex-col justify-center items-center>
+    <div m-4>
+      <img src="https://avatars.githubusercontent.com/u/69113276" alt="avatar" width="120" rounded-60px />
+    </div>
+    <div>
+      <ol flex gap-4>
+        <li>
+          <a href="https://blog.gtn1024.me" target="_blank" w-48px block text-center border-b="1px solid #555">Blog</a>
+        </li>
+        <li>
+          <a href="https://github.com/gtn1024" target="_blank" w-64px block text-center border-b="1px solid #555">GitHub</a>
+        </li>
+      </ol>
+    </div>
+  </div>
+</template>
