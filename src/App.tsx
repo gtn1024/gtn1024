288c770582581e56eb2a95a2d4b91ccd1bd39d43
Taoning Ge <i@gtn1024.me>
2024-05-02 16:17:09 +0800

    finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,26 @@
+import { Button } from './components/ui/button'
+
 function App() {
   return (
-    <div>
+    <div className="h-screen">
+      <div className="flex justify-center items-center h-full bg-gray-100">
+        <div className="">
+          <div className="mb-4">
+            <img className="rounded-[50%] mx-auto" src="https://github.com/gtn1024.png" alt="Avatar" />
+          </div>
+          <div className="flex justify-between">
+            <Button variant="link" asChild>
+              <a href="mailto:i@gtn1024.me">Contact</a>
+            </Button>
+            <Button variant="link" asChild>
+              <a href="https://github.com/gtn1024">GitHub</a>
+            </Button>
+            <Button variant="link" asChild>
+              <a href="https://blog.gtn1024.me">Blog</a>
+            </Button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
