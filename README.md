7be022559587d7ed86018c8bcff13593ee2ec3c4
Taoning Ge <gtn1024@foxmail.com>
2024-09-17 14:40:38 +0800

    update

---
+++ README.md
@@ -0,0 +1,31 @@
+### Hi there 👋
+
+**关于我：**
+
+- Email: <a href="mailto:gtn1024@foxmail.com" target="_blank">Send Email</a>
+- Homepage: <https://gtn1024.me>
+- Resume：[要不来看一下?~](https://github.com/gtn1024/resume/blob/master/resume.pdf)
+
+<table>
+<tbody>
+<tr>
+<td>
+
+[![GitHub stats](https://github-readme-stats.vercel.app/api?username=gtn1024&show_icons=true&include_all_commits=true&theme=buefy&hide_border=true)](https://github.com/gtn1024)
+
+</td>
+<td>
+
+[![GitHub stats](https://github-readme-stats.vercel.app/api/top-langs/?username=gtn1024&layout=compact&theme=buefy&hide_border=true)](https://github.com/gtn1024)
+
+</td>
+</tr>
+</tbody>
+</table>
+
+**我使用的语言：**
+
+![Java](https://img.shields.io/badge/Java-000000?style=flat-square&logo=openjdk&logoColor=white)
+![Kotlin](https://img.shields.io/badge/Kotlin-000000?style=flat-square&logo=kotlin&logoColor=white)
+![Nodejs](https://img.shields.io/badge/Nodejs-000000?style=flat-square&logo=nodedotjs&logoColor=white)
+![Cangjie](https://img.shields.io/badge/Cangjie-000000?style=flat-square&logo=cangjie&logoColor=white)
--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Button } from './components/ui/button'
 function App() {
   return (
     <div className="h-screen">
-      <div className="flex justify-center items-center h-full bg-gray-100">
+      <div className="flex justify-center items-center h-full bg-gray-100 flex-col gap-2">
         <div className="">
           <div className="mb-4">
             <img className="rounded-[50%] mx-auto" src="https://github.com/gtn1024.png" alt="Avatar" />
@@ -18,7 +18,16 @@ function App() {
             <Button variant="link" asChild>
               <a href="https://blog.gtn1024.me">Blog</a>
             </Button>
+            <Button variant="link" asChild>
+              <a href="https://github.com/gtn1024/resume/blob/master/resume.pdf">Resume</a>
+            </Button>
           </div>
+        </div>
+        <div className='flex gap-2'>
+          <img src="https://img.shields.io/badge/Java-000000?style=flat-square&logo=openjdk&logoColor=white" alt="Java" />
+          <img src="https://img.shields.io/badge/Kotlin-000000?style=flat-square&logo=kotlin&logoColor=white" alt="Kotlin" />
+          <img src="https://img.shields.io/badge/Nodejs-000000?style=flat-square&logo=nodedotjs&logoColor=white" alt="Nodejs" />
+          <img src="https://img.shields.io/badge/Cangjie-000000?style=flat-square&logo=cangjie&logoColor=white" alt="Cangjie" />
         </div>
       </div>
     </div>
